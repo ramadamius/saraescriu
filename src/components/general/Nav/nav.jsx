dfbb250ab4d53eb497e7606a5599a2f91e307650
ramadamius <ramadamius@gmail.com>
2020-12-06 00:39:16 -0300

    Added responsive menu

--- src/components/general/Nav/nav.jsx
+++ src/components/general/Nav/nav.jsx
@@ -9,12 +9,12 @@ function Nav() {
                     <a href="#" className="col-12 text-center"><img className="img-fluid" src={logo} alt="Sara escriu logo"/></a>
                 </div>
 
-                <nav className="row">
-                    <ul className="row col-12">
-                        <a className="col-3 mx-auto" href="#"><li className="txt-center">About us</li></a>
-                        <a className="col-3 mx-auto" href="#"><li className="txt-center">Shop now</li></a>
-                        <a className="col-3 mx-auto" href="#"><li className="txt-center">Blog zone</li></a>
-                        <a className="col-3 mx-auto" href="#"><li className="txt-center">Contact us</li></a>
+                <nav className="navbar navbar-expand-md justify-content-center">
+                    <ul className="navbar-nav container-fluid">
+                        <li className="nav-item mx-auto"><a href="#" className="nav-link text-center">About us</a></li>
+                        <li className="nav-item mx-auto"><a href="#" className="nav-link text-center">Shop now</a></li>
+                        <li className="nav-item mx-auto"><a href="#" className="nav-link text-center">Blog zone</a></li>
+                        <li className="nav-item mx-auto"><a href="#" className="nav-link text-center">Contact us</a></li>
                     </ul>
                 </nav>
             </header>
